b92c7e93f8eb13131bfa38f198dfd7f674ddcbe1
Yash Charde <Yash.C@Orioninc.com>
2026-01-30 10:36:01 +0530

    small change in function

--- database/tools.py
+++ database/tools.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from database.models import SupportTickets
 
 # Here we will add tools 
-def save_ticket_to_db(session: Session, email: str, problem: str)
+def save_ticket_to_db(session: Session, email: str, problem: str):
     new_ticket = SupportTickets(user_email = email, description = problem)
     session.add(new_ticket)
     session.commit()
